8e3494ba6329c1aea45fd613deef8f5b9a83ba7a
Dave Martis <dmartis@myseneca.ca>
2024-05-21 13:08:07 -0400

    lab2e done

---
+++ lab2e.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+#Author: Dave Martis
+#Author ID: 154861207
+#Date Created: 2024/05/11
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print('blast off!')
